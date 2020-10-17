1b8326fb3c37b49b687a0d4b447992300ddc1c37
Inspired9k <72418473+Inspired9k@users.noreply.github.com>
2020-10-17 23:31:10 +0200

    Add files via upload

---
+++ лаба7.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+void swap(int &a, int &b) {
+    int t = a;
+    a = b;
+    b = t;
+}
+int main()
+{
+    const int N = 5;
+    int mas[N];
+    bool flag = false;
+    for(int i = 0; i < N; i++) {
+        scanf_s("%d", &mas[i]);
+        if ((mas[i] % 10) + (mas[i] / 10) == 15) {
+            flag = true;
+           
+        }   
+
+    }
+    if (flag == false) {
+        for (int i = 0; i < N; i++)
+            for (int j = i+1; j < N; j++) {
+                if (mas[i] > mas[j]) {
+                    swap(mas[i], mas[j]);
+                }
+                else
+                    continue;
+            }
+            
+        
+    }
+    for (int i = 0; i < N; i++){
+           printf(" %d ", mas[i]);
+    }
+     
+    
+}
+
